4d05890c9b94b830b2cb4ed81ada33a524f63d82
mabrursatori <mabrursatori@gmail.com>
2020-07-16 08:15:59 +0700

    Membuat fitur registrasi(membuat database user dan file registrasi.php)

--- functions.php
+++ functions.php
@@ -108,7 +108,38 @@ function ubah($data)
 
 function cari($keyword)
 {
+
     $query = "SELECT * FROM mahasiswa WHERE nama LIKE '%$keyword%' OR nrp LIKE '%$keyword%' OR email LIKE '%$keyword%' OR jurusan LIKE '%$keyword%'";
 
     return query($query);
+}
+
+function registrasi($data)
+{
+    global $conn;
+    $username = strtolower(stripslashes($data['username']));
+    $password = mysqli_real_escape_string($conn, $data['password']);
+    $password2 = mysqli_real_escape_string($conn, $data['password2']);
+
+    $result = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username'");
+
+    if (mysqli_fetch_assoc($result)) {
+        echo "<script>
+        alert('username yang anda masukan sudah terpakai');
+        </script>";
+        return false;
+    }
+
+    if ($password === $password2) {
+        echo "<script>
+        alert('konfigurasi password anda tidak sama');
+        </script>";
+        return false;
+    }
+
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    mysqli_query($conn, "INSERT INTO user VALUES ('', '$username', '$password')");
+
+    return mysqli_affected_rows($conn);
 }
---
+++ registrasi.php
@@ -0,0 +1,47 @@
+<?php
+require 'function.php';
+
+if (isset($_POST['register'])) {
+    if (registrasi($_POST) > 0) {
+        echo "<script>
+        alert('Berhasil Registrasi');
+        </script>";
+    } else {
+        echo mysqli_error($conn);
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <label for="password2">Konfigurasi Password</label>
+                <input type="password" name="password2" id="password2">
+            </li>
+            <li>
+                <button type="submit" name="register">Regiter</button>
+            </li>
+        </ul>
+    </form>
+</body>
+
+</html>
